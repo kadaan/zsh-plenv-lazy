b203bb3141a83db8584bc5c74a41ebef91b0e828
Joel Baranick <joel.baranick@ensighten.com>
2019-03-11 16:32:39 -0700

    Fix completions location, now requires greadlink

--- plenv-lazy.plugin.zsh
+++ plenv-lazy.plugin.zsh
@@ -2,7 +2,7 @@
 if type plenv &> /dev/null; then
   local P;ENV_SHIMS="${PLENV_ROOT:-${HOME}/.plenv}/shims"
   export PATH="${PLENV_SHIMS}:${PATH}"
-  source $(whence -p plenv)/../../completions/plenv.zsh
+  source $(dirname $(greadlink -f `whence -p plenv`))/../completions/plenv.zsh
   function plenv() {
     unset -f plenv > /dev/null 2>&1
     eval "$(command plenv init -)"
